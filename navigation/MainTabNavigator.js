92a6309847a5ce2ba7392f545402420fc1f3e382
Thomas <blabla928@hotmail.com>
2018-12-10 14:20:47 +0100

    initial upload

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import TeamRedScreen from '../screens/TeamRedScreen';
+import TeamBlueScreen from '../screens/TeamBlueScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+
+const TeamRedStack = createStackNavigator({
+  TeamRed: TeamRedScreen,
+});
+
+TeamRedStack.navigationOptions = {
+  tabBarLabel: 'Team Red',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-bonfire`
+          : 'md-bonfire'
+      }
+    />
+  ),
+};
+
+const TeamBlueStack = createStackNavigator({
+  TeamBlue: TeamBlueScreen,
+});
+
+TeamBlueStack.navigationOptions = {
+  tabBarLabel: 'Team Blue',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-water`
+          : 'md-water'
+      }
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  TeamRedStack,
+  TeamBlueStack
+});
---
+++ screens/TeamBlueScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+
+export default class TeamBlueScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <Text style={styles.textStyle}>{'Hey, det her er jeres første tekst - prøv at finde mig i TeamBlueScreen.js!'}</Text>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    color: "#FFFFFF"
+  },
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#004DE2',
+  },
+});
